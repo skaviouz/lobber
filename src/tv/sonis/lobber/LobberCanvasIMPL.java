4114428449747cabbc5e4ee7771cd48ec4779007
skaviouz <>
2013-05-17 17:10:52 -0700

    routine commit+push/pull new app > tv.sonis.lobber.LobberPlainApplication loads LobberCanvasIMPL

--- src/tv/sonis/lobber/LobberCanvasIMPL.java
+++ src/tv/sonis/lobber/LobberCanvasIMPL.java
@@ -4,10 +4,12 @@
  */
 package tv.sonis.lobber;
 
+import javax.swing.JPanel;
+
 /**
  *
  * @author skaviouz
  */
-public class LobberCanvasIMPL {
+public class LobberCanvasIMPL extends JPanel {
     
 }
