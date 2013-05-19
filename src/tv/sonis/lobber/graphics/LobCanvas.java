28615041ad4f9e41e5d8a7d6552f99b0d12796eb
skaviouz <gammafreak@msn.com>
2013-05-18 18:16:07 -0700

    routine

--- src/tv/sonis/lobber/graphics/LobCanvas.java
+++ src/tv/sonis/lobber/graphics/LobCanvas.java
@@ -7,6 +7,7 @@ package tv.sonis.lobber.graphics;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 import javax.swing.JComponent;
 
 /**
@@ -17,10 +18,17 @@ public class LobCanvas extends JComponent {
 
     @Override
     public void paintComponent(Graphics g) {
+        int y = getHeight();
+        int x = getWidth();
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g.create();
         g2d.setColor(Color.red);
         g2d.drawString("Hello", 50, 50);
+        
+        g2d.setColor(Color.red);
+        Rectangle2D r2d = new Rectangle2D.Double(15, 5, x-5, y-5);
+        g2d.draw(r2d);
+        
         g2d.dispose();
     }
 }
---
+++ src/tv/sonis/lobber/util/lobberstatics.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tv.sonis.lobber.util;
+
+import tv.sonis.lobber.LobberApplication;
+
+/**
+ *
+ * @author skaviouz
+ */
+public class lobberstatics {
+    
+    public static LobberApplication lobapp = null;
+    
+}
