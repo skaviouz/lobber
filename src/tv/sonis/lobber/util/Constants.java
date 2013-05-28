a9594c0105b5adae0bfcbf5ad8cb6b9a866eebad
skaviouz <gammafreak@msn.com>
2013-05-28 16:45:13 -0700

    math functions, LobberApp uses static import

--- src/tv/sonis/lobber/util/Constants.java
+++ src/tv/sonis/lobber/util/Constants.java
@@ -14,4 +14,25 @@ public class Constants {
         return "Lobber Tools " + Version.var;
     }
     public static final Version Version = new Version("6");
+    
+    public static double random(double max){
+        return random(0, max);
+    }
+    
+    public static double random(double min, double max){
+        return (Math.random()*(max-min) + min);
+    }
+    
+    public static float d2f(double d){
+        return (float) d;
+    }
+    
+    public static int d2i(double d){
+        return (int) d;
+    }
+    
+    public static int f2i(float f){
+        return (int) f;
+    }
+    
 }
