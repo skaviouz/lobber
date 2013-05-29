03cc6d3e9558e0a906cca8d350619c813f38e1c1
skaviouz <gammafreak@msn.com>
2013-05-28 19:39:27 -0700

    converting to multiple classes for ease of developing

--- src/tv/sonis/lobber/LobberApp.java
+++ src/tv/sonis/lobber/LobberApp.java
@@ -12,6 +12,7 @@ import java.awt.Graphics2D;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.event.MouseMotionAdapter;
+import java.awt.image.BufferedImage;
 import javax.swing.*;
 import tv.sonis.lobber.util.Constants;
 import static tv.sonis.lobber.util.Constants.random;
@@ -25,13 +26,7 @@ import static tv.sonis.lobber.util.Constants.f2i;
  */
 public class LobberApp {
 
-    public static GUI gui;
-    public static boolean ShowUI = true;
-    public static boolean[] tempvar = new boolean[]{
-        true, true, true, true, true,
-        true, true, true, true, true,
-        true, true, true, true, true,
-        true, true, true, true, true};
+    public static LFrame gui;
 
     /*
      * Program starts here
@@ -55,108 +50,7 @@ public class LobberApp {
      * MainGUI
      */
     public void createAndShowGUI() {
-        gui = new GUI("Swing Paint Demo");
-    }
-
-    public class GUI extends JFrame {
-
-        public JLayeredPane jlp;
-
-        public void GUI_Resize(ComponentEvent evt) {
-            Component c = (Component) evt.getSource();
-        }
-
-        public GUI(String str) {
-            super(str);
-            settings:
-            {
-                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                setSize(800, 600);
-                addComponentListener(new ComponentListener() {
-                    public void componentResized(ComponentEvent evt) {
-                        GUI_Resize(evt);
-                    }
-
-                    @Override
-                    public void componentMoved(ComponentEvent e) {
-                    }
-
-                    @Override
-                    public void componentShown(ComponentEvent e) {
-                    }
-
-                    @Override
-                    public void componentHidden(ComponentEvent e) {
-                    }
-                });
-            }
-            final int x = getWidth(), y = getHeight();
-            setTitleBarInfo:
-            {
-                setTitle(Constants.getTitle());
-                ImageIcon II = new ImageIcon("./resources/images/icon32_S.png");
-                setIconImage(II.getImage());
-            }
-            insideComponents:
-            {
-                jlp = new JLayeredPane();
-                jlp.setPreferredSize(new Dimension(getWidth(), getHeight()));
-                jlp.setBorder(BorderFactory.createTitledBorder(
-                        "Move the Mouse to Move Duke"));
-                jlp.addMouseMotionListener(new MouseMotionAdapter() {
-                });
-                setContent:
-                {
-                    for (int i = 0; i < 412; i++) {
-                        final int i2 = i;
-                        jlp.add(new JLabel() {
-                            {
-                                setVerticalAlignment(JLabel.TOP);
-                                setHorizontalAlignment(JLabel.CENTER);
-                                setOpaque(true);
-                                setBackground(new Color(d2i(random(254)), d2i(random(254)), d2i(random(254)), 255));
-                                setForeground(Color.black);
-                                setBorder(BorderFactory.createLineBorder(Color.black));
-                                //setBounds(33 + (i2 * 10), 33 + (i2 * 10), 140, 140);
-                                setBounds(d2i(random(2, x - 144)), 22 + d2i(random(2, y - 144)), 140, 140);
-                            }
-                        }, i2 + 10);
-                    }
-                }
-            }
-            copyright:
-            {
-                //Layer1 obviously... =D
-                jlp.add(new JLabel() {
-                    {
-                        setVerticalAlignment(JLabel.TOP);
-                        setHorizontalAlignment(JLabel.CENTER);
-                        setOpaque(true);
-                        setBackground(new Color(255, 255, 255, 0));
-                        setBounds(x - 99, y - 16, 98, 15);
-                    }
-
-                    @Override
-                    protected void paintComponent(Graphics g) {
-                        super.paintComponent(g);
-                        Graphics2D g2d = (Graphics2D) g;
-                        g2d.setColor(new Color(0, 0, 0, 50));
-                        g2d.drawString("\u00a9" + " Skaviouz 2009", getWidth() - 95, getHeight() - 3);
-                    }
-                }, 1);
-            }
-            screen:
-            {
-                jlp.setOpaque(true); //content panes must be opaque
-                setContentPane(jlp);
-                pack();
-            }
-            setScreenLocationInfo:
-            {
-                setLocationByPlatform(true);
-                setVisible(true);
-            }
-        }
+        gui = new LFrame("Swing Paint Demo");
     }
 
     class MyLobberPane extends JPanel {
